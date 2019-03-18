d44da6d6d23fab3147f30760b58e9344efbf11de
a-n-n-i-e <annie.omori@gmail.com>
2019-03-18 15:10:10 +0900

    added sample code

---
+++ TranslatorLineBot/TranslatorLineBot/EchoWithCounterBot.cs
@@ -0,0 +1,152 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace EchoBot
+{
+    /// <summary>
+    /// Represents a bot that processes incoming activities.
+    /// For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
+    /// This is a Transient lifetime service.  Transient lifetime services are created
+    /// each time they're requested. For each Activity received, a new instance of this
+    /// class is created. Objects that are expensive to construct, or have a lifetime
+    /// beyond the single turn, should be carefully managed.
+    /// For example, the <see cref="MemoryStorage"/> object and associated
+    /// <see cref="IStatePropertyAccessor{T}"/> object are created with a singleton lifetime.
+    /// </summary>
+    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1"/>
+    public class EchoWithCounterBot : IBot
+    {
+        private readonly EchoBotAccessors _accessors;
+        private readonly ILogger _logger;
+
+        // Translator Text API の設定
+        private readonly string translatorEndpoint = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=ja";
+        private readonly string translatorSubscriptionKey = "YOUR_SUBSCRIPTION_KEY";
+
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        /// <param name="conversationState">The managed conversation state.</param>
+        /// <param name="loggerFactory">A <see cref="ILoggerFactory"/> that is hooked to the Azure App Service provider.</param>
+        /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1#windows-eventlog-provider"/>
+        public EchoWithCounterBot(ConversationState conversationState, ILoggerFactory loggerFactory)
+        {
+            if (conversationState == null)
+            {
+                throw new System.ArgumentNullException(nameof(conversationState));
+            }
+
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _accessors = new EchoBotAccessors(conversationState)
+            {
+                CounterState = conversationState.CreateProperty<CounterState>(EchoBotAccessors.CounterStateName),
+            };
+
+            _logger = loggerFactory.CreateLogger<EchoWithCounterBot>();
+            _logger.LogTrace("Turn start.");
+        }
+
+        /// <summary>
+        /// Every conversation turn for our Echo Bot will call this method.
+        /// There are no dialogs used, since it's "single turn" processing, meaning a single
+        /// request and response.
+        /// </summary>
+        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
+        /// for processing this conversation turn. </param>
+        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
+        /// <seealso cref="BotStateSet"/>
+        /// <seealso cref="ConversationState"/>
+        /// <seealso cref="IMiddleware"/>
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Handle Message activity type, which is the main activity type for shown within a conversational interface
+            // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
+            // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                // Get the conversation state from the turn context.
+                var newState = await _accessors.CounterState.GetAsync(turnContext, () => new CounterState());
+
+                // Bump the turn count for this conversation.
+                newState.TurnCount++;
+
+                // Set the property using the accessor.
+                await _accessors.CounterState.SetAsync(turnContext, newState);
+
+                // Save the new turn count into the conversation state.
+                await _accessors.ConversationState.SaveChangesAsync(turnContext);
+
+                //// Echo back to the user whatever they typed.
+                //var responseMessage = $"Turn {state.TurnCount}: You sent '{turnContext.Activity.Text}'\n";
+                //await turnContext.SendActivityAsync(responseMessage);
+
+                /// ユーザー入力を翻訳して返す
+                // ユーザー入力の取得
+                var body = new object[] { new { Text = turnContext.Activity.Text } };
+                var requestBody = JsonConvert.SerializeObject(body);
+
+                // Translator Text API の利用
+                using (var client = new HttpClient())
+                using (var request = new HttpRequestMessage())
+                {
+                    request.Method = HttpMethod.Post;
+                    request.RequestUri = new Uri(translatorEndpoint);
+                    request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                    request.Headers.Add("Ocp-Apim-Subscription-Key", translatorSubscriptionKey);
+                    var response = await client.SendAsync(request);
+
+                    // 結果の取得とデシリアライズ
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<List<TranslatorResult>>(jsonResponse);
+
+                    // 返答にセット
+                    var responseMessage =
+                        $"{newState.TurnCount}:「{result[0].translations[0].text}」って言ったね。" +
+                        $"(検出言語: {result[0].detectedLanguage.language})";
+                    await turnContext.SendActivityAsync(responseMessage);
+                }
+
+            }
+            else
+            {
+                //await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected");
+            }
+        }
+
+        // Translator API クラス(JSON デシリアライズ用)
+        public class TranslatorResult
+        {
+            public Detectedlanguage detectedLanguage { get; set; }
+            public List<Translation> translations { get; set; }
+        }
+
+        public class Detectedlanguage
+        {
+            public string language { get; set; }
+            public float score { get; set; }
+        }
+
+        public class Translation
+        {
+            public string text { get; set; }
+            public string to { get; set; }
+        }
+    }
+}
